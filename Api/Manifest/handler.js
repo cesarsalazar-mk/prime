f152902bd3c6d341013090068d53912ff0ad3fee
luisdeleon-mk <ledr1993@gmail.com>
2026-09-11 01:13:32 -0600

    update - Labels

--- Api/Manifest/handler.js
+++ Api/Manifest/handler.js
@@ -130,6 +130,13 @@ module.exports.exportManifest = async event => {
       result && result[0]
         ? result.map(row => {
             const mapped = Object.keys(row).reduce((r, k) => {
+              if (k === 'status') {
+                return {
+                  ...r,
+                  [k]: row[k] === 'Retenido' ? 'Fuerza Tarea' : row[k],
+                }
+              }
+
               if (k === 'weight') {
                 return {
                   ...r,
